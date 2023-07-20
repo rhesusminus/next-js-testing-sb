f2c187b2e16b468e3c5beafa91b797d3ac1c87ee
rhesusminus <erik.niilivirta@gmail.com>
2023-07-20 15:08:30 +0300

    Remove unnecessary import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import useSWR from 'swr'
 import { css } from '../../styled-system/css'
 import styles from './page.module.css'
 import { API_PATHS, getPeople } from '@/api'
-import { useEffect } from 'react'
 
 export default function Home() {
   const { data, isLoading } = useSWR(API_PATHS.PEOPLE, getPeople)
---
+++ src/app/people/page.tsx
@@ -0,0 +1,5 @@
+import { divider } from '../../../styled-system/patterns'
+
+export default function Peope() {
+  return <div>People</div>
+}
